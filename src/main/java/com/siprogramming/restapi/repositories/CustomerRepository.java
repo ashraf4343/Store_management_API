aaf3bd5a93512d25c2d0df8168db9aaf680807dc
md ashrafuzzaman <mdashrafuzzaman4343@gmail.com>
2024-03-01 12:59:06 +0600

    second commit

--- src/main/java/com/siprogramming/restapi/repositories/CustomerRepository.java
+++ src/main/java/com/siprogramming/restapi/repositories/CustomerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.siprogramming.restapi.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    // Additional custom queries can be added here if needed
+
 }
--- src/main/java/com/siprogramming/restapi/repositories/OrderRepository.java
+++ src/main/java/com/siprogramming/restapi/repositories/OrderRepository.java
@@ -1,18 +1,13 @@
 package com.siprogramming.restapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.siprogramming.restapi.entity.Customer;
 import com.siprogramming.restapi.entity.Order;
 
 import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.siprogramming.restapi.entity.Customer;
-import com.siprogramming.restapi.entity.Order;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -28,9 +23,12 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
 
     
- 
-    
- 
-    Double findTotalSaleAmountByOrderDate(LocalDate orderDate);
+    @Query("SELECT o.orderDate FROM Order o WHERE o.orderDate BETWEEN :startDate AND :endDate " +
+            "GROUP BY o.orderDate ORDER BY COUNT(o.orderDate) DESC, o.orderDate DESC")
+    List<LocalDate> findMaxSaleDayInTimeRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+
+    @Query("SELECT SUM(o.amount) FROM Order o WHERE o.orderDate = :orderDate")
+    Double findTotalSaleAmountByOrderDate(@Param("orderDate") LocalDate orderDate);
 }
 
